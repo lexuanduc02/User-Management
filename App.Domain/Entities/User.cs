347e7419be8221f7ccf6b951ccbf3a30140746fa
Daniel W. Le <lexuanduc.hn@gmail.com>
2024-08-12 16:42:34 +0700

    create_database

---
+++ App.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using App.Domain.Entities.Contracts;
+
+namespace App.Domain.Entities
+{
+    public class User : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Salt { get; set; }
+    }
+}
